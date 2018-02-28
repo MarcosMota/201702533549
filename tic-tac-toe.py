94999f311688c02ed783bbb15b743bc2a1ba1cfc
Marcos Mota <marcos.mota287@gmail.com>
2018-02-28 00:45:49 +0000

    Adicionando metodo para checar vitoria

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -2,6 +2,27 @@ import sys
 import time
 import getopt
 
+# Verifica se usuario ganhou
+def check_board(board):
+    profundidade = 0
+    board = ''.join(board)
+    for i in range(len(board)):
+        if board[i] == "_":
+            profundidade = profundidade + 1
+
+    print("Profundidade: {}".format(profundidade))
+    ganhou(board)
+    # Se foi concluido
+    if "_" not in board:
+        print("concluido")
+
+def ganhou(board):
+    list_vitoria = (
+        [6, 7, 8], [3, 4, 5], [0, 1, 2], [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6],
+    )
+
+# Printa o quadro com as jogas
 def print_board(board):
     board = ''.join(board)
     if len(board) == 9:
@@ -33,6 +54,7 @@ if __name__ == "__main__":
                 sys.exit()
             elif opt in ("-v", "--verbose"):
                 print_board(board)
+                check_board(board)
             elif opt in ("-b", "--board"):
                 if len(arg) == 9:
                     board = list(arg)
